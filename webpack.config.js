391ebcb7a6a5a018226f48b780a38429f17a9f50
JustWaveThings <justwavethings@proton.me>
2023-02-22 17:06:03 -0500

    Updated webpack template to remove the bloat

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin =
-	require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-const CircularDependencyPlugin = require('circular-dependency-plugin');
-const DashboardPlugin = require('webpack-dashboard/plugin');
 
 module.exports = {
 	mode: 'development',
@@ -17,7 +12,6 @@ module.exports = {
 		allowedHosts: ['.preview.csb.app', 'localhost'],
 		// .preview.csb.app is the domain for CodeSandbox.io
 		static: './dist',
-		port: auto,
 	},
 	output: {
 		filename: '[name].bundle.js',
@@ -30,28 +24,7 @@ module.exports = {
 			template: './src/index.html',
 			//favicon: path.resolve(__dirname, '../icons/favicon.ico'),
 		}),
-		new BundleAnalyzerPlugin({
-			analyzerPort: auto,
-			defaultSizes: 'stat',
-		}),
-		new DuplicatePackageCheckerPlugin(),
 		new ESLintPlugin(),
-		new CircularDependencyPlugin({
-			// exclude detection of files based on a RegExp
-			exclude: /a\.js|node_modules/,
-			// include specific files based on a RegExp
-			include: /dir/,
-			// add errors to webpack instead of warnings
-			failOnError: true,
-			// allow import cycles that include an asyncronous import,
-			// e.g. via import(/* webpackMode: "weak" */ './file.js')
-			allowAsyncCycles: false,
-			// set the current working directory for displaying module paths
-			cwd: process.cwd(),
-		}),
-		new DashboardPlugin({
-			port: auto,
-		}),
 	],
 	optimization: {
 		runtimeChunk: 'single',
